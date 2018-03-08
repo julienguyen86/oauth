26cff398c30fd9faeff6cce8e264d601785f851f
Frank Valdez <fvaldez421@gmail.com>
2018-03-07 17:08:54 -0800

    working version of OAuth

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 var express = require('express');
 var authRoutes = require('./routes/auth-routes');
+var profileRoutes = require('./routes/profile-routes');
 var db = require("./models");
-
+var cookieSession = require("cookie-session");
 var passportSetup = require('./config/passport-setup');
-
-
+var passport = require("passport");
+var keys = require('./config/keys');
 var app = express();
-
-
 // set up view engine
 app.set('view engine', 'ejs');
+
+app.use(cookieSession({
+	maxAge: 24*60*60*1000,
+	keys: [keys.session.cookieKey]
+}));
+
+//initialize passport
+app.use(passport.initialize());
+
+app.use(passport.session());
+
 
 // set up routes
 app.use('/auth', authRoutes);
 
+app.use("/profile", profileRoutes);
+
 // create home route
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
 res.render('home');
 });
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({}).then(function() {
 app.listen(3000,() => {
 	console.log('app now listening for requests on port 3000')
 });
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,13 +1,15 @@
 var router = require('express').Router();
 var passport = require('passport');
+var path = require("path");
 
 // auth login
-router.get('/login', (req, res) => {
-  res.render('login');
+router.get('/login', function(req, res) {
+  res.render('login', {user: req.user});
 });
 
-router.get('/logout', (req,res) => {
-	res.send('logging out');
+router.get('/logout', function(req,res) {
+	req.logout();
+	res.redirect("/");
 })
 
 
@@ -18,27 +20,15 @@ router.get('/google', passport.authenticate('google',{
 }));
 
 
-//  (req, res) => {
-// 	// handle with passport
-// 	res.send('logging in with google');
-// });
+ (req, res) => {
+	// handle with passport
+	res.send('logging in with google');
+};
 
 
-router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-	res.send('you reached the callback URI');
-
+router.get('/google/redirect', passport.authenticate('google'), function(req, res) {
+	res.redirect("/profile");
 });
 
 
-// router.get('/auth/google/get',
-//   passport.authenticate('google', { scope: 
-//     [ 'https://www.googleapis.com/auth/plus.login',
-//     , 'https://www.googleapis.com/auth/plus.profile.emails.read' ] }
-// ));
-
-// router.get( '/auth/google/callback', 
-//     passport.authenticate( 'google', { 
-//         successRedirect: '/auth/google/success',
-//         failureRedirect: '/auth/google/failure'
-// }));
 module.exports = router;
